6f2e2830ed092f5b716e5e0a7f72e411edf1d11b
Kaic Salomão <imkaicsm@gmail.com>
2024-10-18 17:46:51 -0300

    Adicionado paralelismo no processo de treinamento da rede neural

--- src/model.rs
+++ src/model.rs
@@ -4,7 +4,7 @@ use crate::layers::DenseLayer;
 use crate::loss::Loss;
 use crate::optimizer::Optimizer;
 use crate::visualization::TrainingStats;
-use ndarray::{s, Array2};
+use ndarray::{parallel::prelude::*, s, Array2};
 use serde::{Deserialize, Serialize};
 use std::sync::{Arc, Mutex};
 use std::{fs::File, io::Read, io::Write};
@@ -21,7 +21,13 @@ impl NeuralNetwork {
         NeuralNetwork { layers: Vec::new() }
     }
 
-    /// Salva o modelo utilizando a serialização binária `bincode` para melhor performance.
+    /// Salva o modelo utilizando a serialização binária `bincode` para melhorar a performance.
+    ///
+    /// # Parâmetros
+    /// - `filename`: O caminho onde o modelo será salvo.
+    ///
+    /// # Retornos
+    /// - `Result<(), Box<dyn std::error::Error>>`: Retorna Ok se o modelo foi salvo com sucesso, ou um erro caso contrário.
     pub fn save(&self, filename: &str) -> Result<(), Box<dyn std::error::Error>> {
         let encoded: Vec<u8> = bincode::serialize(&self)?;
         let mut file = File::create(filename)?;
@@ -29,14 +35,20 @@ impl NeuralNetwork {
         Ok(())
     }
 
-    /// Carrega o modelo a partir de um arquivo serializado com `bincode`.
+    /// Carrega um modelo a partir de um arquivo serializado com `bincode`.
+    ///
+    /// # Parâmetros
+    /// - `filename`: O caminho de onde o modelo será carregado.
+    ///
+    /// # Retornos
+    /// - `Result<NeuralNetwork, Box<dyn std::error::Error>>`: A rede neural carregada ou um erro, caso a operação falhe.
     pub fn load(filename: &str) -> Result<NeuralNetwork, Box<dyn std::error::Error>> {
         let mut file = File::open(filename)?;
         let mut buffer = Vec::new();
         file.read_to_end(&mut buffer)?;
         let mut deserialized: NeuralNetwork = bincode::deserialize(&buffer)?;
 
-        // Restaura as ativações nas camadas após desserialização
+        // Restaura as ativações nas camadas após a desserialização
         for layer in &mut deserialized.layers {
             layer.restore_activation();
         }
@@ -45,12 +57,22 @@ impl NeuralNetwork {
     }
 
     /// Adiciona uma nova camada à rede neural.
+    ///
+    /// # Parâmetros
+    /// - `layer`: A nova camada a ser adicionada à rede.
     pub fn add_layer(&mut self, layer: DenseLayer) {
         self.layers.push(layer);
     }
 
-    /// Realiza a passagem forward para um lote de entradas.
-    /// Propaga os dados através de todas as camadas da rede.
+    /// Executa a propagação forward para um lote de entradas.
+    ///
+    /// Os dados são propagados através de todas as camadas da rede.
+    ///
+    /// # Parâmetros
+    /// - `input`: Os dados de entrada para a rede.
+    ///
+    /// # Retornos
+    /// - `Array2<f32>`: A saída gerada pela última camada da rede.
     pub fn forward(&mut self, input: &Array2<f32>) -> Array2<f32> {
         let mut output = input.clone();
         for layer in &mut self.layers {
@@ -59,8 +81,16 @@ impl NeuralNetwork {
         output
     }
 
-    /// Realiza a passagem backward para um lote de erros de saída.
+    /// Executa a retropropagação (backward) para um lote de erros de saída.
+    ///
     /// Atualiza os pesos e vieses de todas as camadas usando o otimizador fornecido.
+    ///
+    /// # Parâmetros
+    /// - `output_error`: O erro da saída que será propagado de volta.
+    /// - `optimizer`: O otimizador que será usado para atualizar os pesos.
+    ///
+    /// # Retornos
+    /// - `Result<(), Box<dyn std::error::Error>>`: Retorna Ok se a operação foi bem-sucedida.
     pub fn backward(
         &mut self,
         output_error: &Array2<f32>,
@@ -73,43 +103,54 @@ impl NeuralNetwork {
         Ok(())
     }
 
-    /// Treina a rede neural usando os dados fornecidos.
+    /// Treina a rede neural utilizando os dados fornecidos.
     ///
-    /// Parâmetros:
-    /// - `x_train`: Dados de entrada para treinamento
-    /// - `y_train`: Rótulos/alvos correspondentes
-    /// - `epochs`: Número de épocas de treinamento
-    /// - `batch_size`: Tamanho do lote para treinamento em mini-lotes
-    /// - `loss_fn`: Função de perda a ser usada
-    /// - `optimizer`: Otimizador para atualização de pesos
-    /// - `stats`: Estrutura para coletar estatísticas de treinamento
+    /// # Parâmetros
+    /// - `neural_net`: A rede neural encapsulada em `Arc<Mutex<Self>>` para permitir acesso concorrente seguro.
+    /// - `x_train`: Dados de entrada para o treinamento.
+    /// - `y_train`: Rótulos/alvos correspondentes.
+    /// - `epochs`: Número de épocas de treinamento.
+    /// - `batch_size`: Tamanho do lote para o treinamento em mini-lotes.
+    /// - `loss_fn`: Função de perda a ser usada, encapsulada em `Arc` para ser segura para threads.
+    /// - `optimizer`: Otimizador para a atualização dos pesos, também encapsulado em `Arc<Mutex>` para garantir acesso seguro entre threads.
+    /// - `stats`: Estrutura para coletar estatísticas de treinamento, encapsulada em `Arc<Mutex>`.
     pub fn train(
-        &mut self,
+        neural_net: Arc<Mutex<Self>>,
         x_train: &Array2<f32>,
         y_train: &Array2<f32>,
         epochs: usize,
         batch_size: usize,
-        loss_fn: &dyn Loss,
-        optimizer: &mut dyn Optimizer,
+        loss_fn: Arc<dyn Loss + Sync + Send>,
+        optimizer: Arc<Mutex<dyn Optimizer + Send>>,
         stats: Arc<Mutex<TrainingStats>>,
     ) {
         let n_samples = x_train.shape()[0];
 
         for epoch in 0..epochs {
-            // Itera sobre mini-lotes
-            for i in (0..n_samples).step_by(batch_size) {
-                let end = usize::min(i + batch_size, n_samples);
-                let x_batch = x_train.slice(s![i..end, ..]).to_owned();
-                let y_batch = y_train.slice(s![i..end, ..]).to_owned();
+            // Processa os mini-lotes em paralelo
+            (0..n_samples)
+                .into_par_iter()
+                .step_by(batch_size)
+                .for_each(|i| {
+                    let end = usize::min(i + batch_size, n_samples);
+                    let x_batch = x_train.slice(s![i..end, ..]).to_owned();
+                    let y_batch = y_train.slice(s![i..end, ..]).to_owned();
 
-                // Realiza a passagem forward e backward para o mini-lote
-                let output = self.forward(&x_batch);
-                let error = loss_fn.derivative(&output, &y_batch);
-                self.backward(&error, optimizer).unwrap();
-            }
+                    // Bloqueia o acesso à rede neural para processar o mini-lote
+                    let mut neural_net = neural_net.lock().unwrap();
 
-            // Calcula e registra a perda ao final de cada época
-            let output = self.forward(&x_train);
+                    // Passagem forward e backward
+                    let output = neural_net.forward(&x_batch);
+                    let error = loss_fn.derivative(&output, &y_batch);
+
+                    // Bloqueia o otimizador para atualizar os pesos
+                    let mut optimizer = optimizer.lock().unwrap();
+                    neural_net.backward(&error, &mut *optimizer).unwrap();
+                });
+
+            // Calcula a perda após cada época
+            let mut neural_net = neural_net.lock().unwrap();
+            let output = neural_net.forward(&x_train);
             let loss = loss_fn.loss(&output, &y_train);
 
             // Atualiza as estatísticas de treinamento
